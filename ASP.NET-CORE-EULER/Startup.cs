744edb9226aebf76b2230e2fcd8139cfd77a9d42
nbsteuv <nbsteuv@gmail.com>
2017-05-31 05:10:30 -0400

    Populated FeatureToggles class with values from config.json

--- ASP.NET-CORE-EULER/Startup.cs
+++ ASP.NET-CORE-EULER/Startup.cs
@@ -13,11 +13,24 @@ namespace ASPNET_CORE_EULER
 {
     public class Startup
     {
+        private readonly IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile(env.ContentRootPath + "/config.json")
+                .Build();
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<FeatureToggles>();
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions = configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,11 +39,6 @@ namespace ASPNET_CORE_EULER
             loggerFactory.AddConsole();
 
             app.UseExceptionHandler("/error.html");
-
-            var configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddJsonFile(env.ContentRootPath + "/config.json")
-                .Build();
 
             if (features.EnableDeveloperExceptions)
             {
