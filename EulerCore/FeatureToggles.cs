1cae7b488fba9c49b36f18e90d547aca9fae1a0c
nbsteuv <nbsteuv@gmail.com>
2017-05-31 06:29:59 -0400

    Created new project with the same changes to fix errors

---
+++ EulerCore/FeatureToggles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EulerCore
+{
+    public class FeatureToggles
+    {
+        public bool EnableDeveloperExceptions { get; set; }
+    }
+}
